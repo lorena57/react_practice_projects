d0123f3287400e409d8ac0d218e319132c7fb1ef
github username <github email address>
2021-01-02 17:00:52 -0800

    create additonal component for display

---
+++ seasons/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const App = () => {
+    return (
+        <div>
+            Testing
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
+
